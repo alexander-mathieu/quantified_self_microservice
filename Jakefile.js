488f1b722790e7e40632c8bae474c2c32d10e691
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-24 19:31:38 -0600

    Add recipeSerializer to Jakefile seedDatabase task
    Co-Authored-By: Billy Urrutia <billyurrutia@gmail.com>

--- Jakefile.js
+++ Jakefile.js
@@ -2,20 +2,28 @@ require('dotenv').config();
 
 var fetch = require('node-fetch');
 
+var Recipe = require('./models').Recipe;
+
+var recipeSerializer = require('./serializers/recipe_serializer');
+
 task('seedDatabase', { async: true }, (foodString) => {
   let foodTypes = foodString.split(' ')
 
   for (let i = 0; i <= 2; i++) {
     fetch(`https://api.edamam.com/search?q=${foodTypes[i]}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}`)
     .then(response => {
-      return response.json()
+      return response.json();
     })
     .then(foodData => {
-      foodData.hits.forEach(recipe => {
-        let serializedRecipe = new recipeSerializer(recipe);
+      let foodType = foodData.q
 
-        let recipe = Recipe.build(serializedRecipe);
-        recipe.save;
+      foodData.hits.forEach(recipeData => {
+        let serializedRecipe = new recipeSerializer(foodType, recipeData.recipe);
+
+        return Recipe.create(serializedRecipe)
+        .then(recipe => {
+          console.log(`Created ${recipe.name}.`);
+        })
       })
     })
   }
