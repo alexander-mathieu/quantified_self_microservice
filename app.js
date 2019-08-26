8e5695f5794271877f550cd02380720b4021d7c5
billy urrutia <billyurrutia@gmail.com>
2019-08-26 15:14:54 -0600

    added graphql route

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var graphqlHTTP = require('express-graphql')
 
 var indexRouter = require('./routes/index');
+var schema = require('./graphql/schema');
+var { getRecipes } = require('./graphql/resolvers.js');
 
 var app = express();
 
@@ -19,6 +22,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+var root = {
+  recipes: getRecipes
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
