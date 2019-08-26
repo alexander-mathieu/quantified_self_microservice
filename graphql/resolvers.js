c49614c757c73a13a029375102cf907151a58fb6
billy urrutia <billyurrutia@gmail.com>
2019-08-26 17:56:38 -0600

    created getAverageCalorieCount function created getRecipesByNumOfIngredients created getRecipesByPrepTime

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,6 @@
 var Recipe = require('../models').Recipe;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
 
 module.exports = {
   getRecipes: (params) => {
@@ -6,4 +8,30 @@ module.exports = {
       where: params
     })
   },
+
+  getAverageCalorieCount: (foodType) => {
+    return Recipe.findAll({
+      where: foodType,
+      attributes: ['Recipe.foodType',[Sequelize.fn('avg', Sequelize.col('calorieCount')),'average']],
+      group: ['Recipe.foodType'],
+      raw: true
+    })
+  },
+
+  getRecipesByNumOfIngredients: () => {
+    return Recipe.findAll({
+      order: [['numberOfIngredients']]
+    })
+  },
+
+  getRecipesByPrepTime: () => {
+    return Recipe.findAll({
+      where: {
+        preparationTime: {
+          [Op.gt]: 0
+        }
+      },
+      order: [['preparationTime']]
+    })
+  }
 }
